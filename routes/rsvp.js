96ccf5dbad5ee6e841b2c92036f583a6b631b99b
Karl Chen <klc026@ucsd.edu>
2019-02-28 10:52:53 -0800

    thirdpartyAPI lab

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+
+exports.addRSVP = function(request,response) {
+console.log(rsvpEmail);
+
+data.rsvp.push(rsvpEmail);
+
+response.send(rsvpEmail);
+
+
+};
